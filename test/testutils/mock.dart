bcd8942630138859123fe8c9bcee020f799822ff
Waffiq Aziz <59906896+waffiqaziz@users.noreply.github.com>
2025-02-15 16:35:05 +0700

    test: add more unit and widget test (#22)
    - add more unit test
    - use flutter-version: 3.27.4 on workflow configuration
    - update workflow to prevent generate new comment test coverage each commit

--- test/testutils/mock.dart
+++ test/testutils/mock.dart
@@ -1,9 +1,15 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:http/http.dart' as http;
 import 'package:mocktail/mocktail.dart';
+import 'package:restaurantzz/core/data/local/local_database_service.dart';
 import 'package:restaurantzz/core/data/services/local_notification_service.dart';
 import 'package:restaurantzz/core/data/services/shared_preferences.dart';
+import 'package:restaurantzz/core/data/services/workmanager_service.dart';
 import 'package:restaurantzz/core/networking/services/api_services.dart';
+import 'package:restaurantzz/core/provider/detail/detail_provider.dart';
 import 'package:restaurantzz/core/provider/favorite/local_database_provider.dart';
+import 'package:restaurantzz/core/provider/notification/local_notification_provider.dart';
+import 'package:restaurantzz/core/provider/setting/shared_preferences_provider.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:workmanager/workmanager.dart';
 
@@ -12,6 +18,16 @@ class MockHttpClient extends Mock implements http.Client {}
 class MockApiServices extends Mock implements ApiServices {}
 
 class MockLocalDatabaseProvider extends Mock implements LocalDatabaseProvider {}
+
+class MockDetailProvider extends Mock implements DetailProvider {}
+
+class MockSharedPreferencesProvider extends Mock
+    implements SharedPreferencesProvider {}
+
+class MockLocalNotificationProvider extends Mock
+    implements LocalNotificationProvider {}
+
+class MockLocalDatabaseService extends Mock implements LocalDatabaseService {}
 
 class MockLocalNotificationService extends Mock
     implements LocalNotificationService {}
@@ -22,3 +38,8 @@ class MockDatabaseFactory extends Mock implements DatabaseFactory {}
 
 class MockSharedPreferencesService extends Mock
     implements SharedPreferencesService {}
+
+class MockFlutterLocalNotificationsPlugin extends Mock
+    implements FlutterLocalNotificationsPlugin {}
+
+class MockWorkmanagerService extends Mock implements WorkmanagerService {}
