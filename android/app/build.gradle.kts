f7165ed6f2ab447d4f916b460e1bf8c5c7241de3
WaffiqA <waffiq.a2002@gmail.com>
2026-08-14 20:49:27 +0700

    chore: migrate Flutter Android projects to built-in Kotlin
    - Migrate Flutter Android projects to built-in Kotlin
    - Bump Desugar jdk libs to v2.1.5
    - Bump AGP to v9.0.1
    - Bump Kotlin to v2.3.20

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,7 +1,5 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -14,10 +12,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
         isCoreLibraryDesugaringEnabled = true
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
@@ -40,9 +34,15 @@ android {
 }
 
 dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
     implementation("androidx.window:window:1.0.0")
     implementation("androidx.window:window-java:1.0.0")
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+    }
 }
 
 flutter {
